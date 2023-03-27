44051b37c47f488a3f394e40ad10bf9cdc9489d9
Miguel Alejandro Segura <segu803282@siigo.com>
2023-03-27 02:49:58 -0500

    Se dockeriza la aplicación

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 import { InversifyExpressServer } from 'inversify-express-utils';
 import { buildProviderModule } from 'inversify-binding-decorators';
 import { container } from './inversify.config';
+import mongoose from 'mongoose';
 
 // Importa tus controladores aquí
 import './controllers/UserController';
@@ -20,6 +21,17 @@ app.use(cors());
 
 // Carga los controladores con decoradores @controller
 container.load(buildProviderModule());
+
+// Configura la conexión a MongoDB
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/VelaDb';
+
+mongoose.connect(MONGO_URI, {
+  autoCreate: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
 
 const inversifyServer = new InversifyExpressServer(container, null, { rootPath: '/api' }, app);
 const server = inversifyServer.build();
